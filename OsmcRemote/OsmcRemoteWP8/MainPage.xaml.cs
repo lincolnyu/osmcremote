4c05a91d037a465140666f0415ac6c8edc39c526
Lincoln Yu <quanben@msn.com>
2015-10-01 00:11:03 +1000

    added the initial WP8 project.

---
+++ OsmcRemote/OsmcRemoteWP8/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System.ComponentModel;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace OsmcRemoteWP8
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private double _smallButtonWidth;
+
+        private double _smallButtonHeight;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            NavigationCacheMode = NavigationCacheMode.Required;
+
+            DataContext = this;
+        }
+
+        public double SmallButtonWidth
+        {
+            get
+            {
+                return _smallButtonWidth;
+            }
+            set
+            {
+                if (_smallButtonWidth != value)
+                {
+                    _smallButtonWidth = value;
+                    RaisePropertyChanged("SmallButtonWidth");
+                }
+            }
+        }
+
+        public double SmallButtonHeight
+        {
+            get
+            {
+                return _smallButtonHeight;
+            }
+            set
+            {
+                if (_smallButtonHeight != value)
+                {
+                    _smallButtonHeight = value;
+                    RaisePropertyChanged("SmallButtonHeight");
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="args">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs args)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private void OkButtonSizeChanged(object sender, SizeChangedEventArgs args)
+        {
+            UpdateSmallButtonSize(args.NewSize);
+        }
+
+        private void UpdateSmallButtonSize(Size largeButtonSize)
+        {
+            var w = largeButtonSize.Width / 3;
+            var h = w / 2;
+            SmallButtonWidth = w;
+            SmallButtonHeight = h;
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
